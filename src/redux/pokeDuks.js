219c9c898559f0291aad8239798b1d3431975b1a
Guille Riusech <griusech@users.noreply.github.com>
2020-08-16 10:05:21 -0300

    Configurando Ducks
    Creo el pokeDuks y le paso las constantes, el reducer y las acciones

---
+++ src/redux/pokeDuks.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+
+// Constates
+const dataInicial = {
+    array: []
+}
+
+
+// Types
+const OBTENER_POKE_EXITO = 'OBTENER_POKE_EXITO';
+
+// Reducer
+export default function pokeReducer(state = dataInicial, action) {
+    switch(action.type) {
+        case OBTENER_POKE_EXITO:
+            return {
+                ...state,
+                array: action.payload
+            }
+    }
+}
+
+
+// Acciones
+export const obtenerPokemonesAccion = () => async (dispatch, getState) => {
+    try {
+        const res = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=100&offset=200')3
+        dispatch({
+            type: OBTENER_POKE_EXITO,
+            payload: res.data.results
+        })
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
