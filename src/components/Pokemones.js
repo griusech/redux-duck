984c29cc555d6f5a882bb1cf97480ef4ec2f2324
Guille Riusech <griusech@users.noreply.github.com>
2020-08-16 10:37:01 -0300

    Mostrar en interfaz
    Corregi errores del store e imprimi por la interfaz el componente pokemones

---
+++ src/components/Pokemones.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Pokemones = () => {
+    return (
+        <div>
+            Lista de pokemones
+        </div>
+    )
+}
+
+export default Pokemones
+
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,15 @@
-import { createReducer, combineReducer, compose, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-// Importo el reducer
-import pokeReducer from './pokesDucks'
+import {createStore, combineReducers, compose, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk'
 
-const rootReducer = combineReducer({
-    pokemones: pokeRecuer
+import pokeReducer from './pokeDuks'
+
+const rootReducer = combineReducers({
+    pokemones: pokeReducer
 })
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-export default function generateStore(){
-    const store = createStore( rootReducer, composeEnhancers( applyMiddleware(thunk) ))
-    return store;
+export default function generateStore() {
+    const store = createStore( rootReducer, composeEnhancers( applyMiddleware(thunk) ) )
+    return store
 }
